a6c17a0a8d8fc3d366124106bbce237c8c2d2290
Meriem1551 <meriem200409@gmail.com>
2023-10-06 15:41:14 +0200

    Strings

---
+++ strings.py
@@ -0,0 +1,14 @@
+name = "My name is \"Meriem\""
+name1 = 'My name is \'Meriem\''
+greet = 'hello'
+print(greet[0:3])
+print(greet[-1])
+print(greet[2]) 
+str = 'dudes'
+str1 = greet + ' ' + str
+print(str1)
+print(greet * 3)
+print(greet.upper())
+cheeses = "brie, chedder, stilton"
+print(cheeses.split(','))
+print(len(greet))
