1f04451dc9f52402b45fea2f6386c8d933a44372
Meriem1551 <meriem200409@gmail.com>
2023-10-07 15:53:58 +0200

    Passing default params

--- functions/first.py
+++ functions/first.py
@@ -1,8 +1,12 @@
-def greet(name, time):
+def greet(
+    name="ryu", time="night"
+):  # this should work even if we dont pass params inthe call func; it's called default param
     print(f"Good {time} {name}, hope you are well")
 
 
-name = input("Enter your name: ")
-time = input("Enter the time of day: ")
+# name = input("Enter your name: ")
+# time = input("Enter the time of day: ")
 
-greet(name, time)
+greet()  # if we pass param here it will overwrite the default one
+
+# adding default params
