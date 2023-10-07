2476051b1329edb87d129cfcfabcd56779e4157b
Meriem1551 <meriem200409@gmail.com>
2023-10-07 14:40:26 +0200

    Adding if else statement

--- if-else.py
+++ if-else.py
@@ -1,3 +1,8 @@
 age = int(input("Enter your age: "))
 if age < 10:
     print("You are young, strange one")
+elif age < 40:
+    print("The fire in you is strong, strange one ")
+
+else:
+    print("You are wise beyond doubt, strange one")
