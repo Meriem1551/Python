6af58f43980119c60005db15aa66899c45370737
Meriem1551 <meriem200409@gmail.com>
2023-10-07 15:11:00 +0200

    If within for

--- loops/for.py
+++ loops/for.py
@@ -1,7 +1,13 @@
-ninjas = ["Hori", "Miamura", "Ishikawa", "Sakura"]
+ninjas = ["Hori", "Miyamura", "Ishikawa", "Sakura"]
 
 # for ninja in ninjas:
 #     print(ninja)
 
-for ninja in ninjas[1:3]:
-    print(ninja)
+# for ninja in ninjas[1:3]:  # section of list
+#     print(ninja)
+
+for ninja in ninjas:
+    if ninja == "Miyamura":
+        print(f"{ninja} - black hair")
+    else:
+        print(ninja)
