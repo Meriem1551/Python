46a6f604978683ac2d886237ec60259f03b9818d
Meriem1551 <meriem200409@gmail.com>
2023-10-07 14:10:23 +0200

    Craeting func that calculate area of a circle

---
+++ area_calc.py
@@ -0,0 +1,9 @@
+# name = input('Tell me your name, punk: ')#output to te console
+# age = input('And your age: ')
+# print(name + ' you are ' + age)
+
+# Calc the area of a circle
+
+radius = input("Enter the radius of your circle (m): ")
+area = 3.142 * int(radius) ** 2
+print("The area of your circle is: ", area)
