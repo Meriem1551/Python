2661b5ba6c4ffcc756e00e0e98be00d90ca7bfe9
Meriem1551 <meriem200409@gmail.com>
2023-10-07 14:46:48 +0200

    Using string in if-else

--- if-else.py
+++ if-else.py
@@ -1,8 +1,19 @@
-age = int(input("Enter your age: "))
-if age < 10:
-    print("You are young, strange one")
-elif age < 40:
-    print("The fire in you is strong, strange one ")
+# OPERATORS
+# <=, >=, ==, !=, &&, ||
 
+
+# age = int(input("Enter your age: "))
+# if age < 10:
+#     print("You are young, strange one")
+# elif age < 40:
+#     print("The fire in you is strong, strange one ")
+
+# else:
+#     print("You are wise beyond doubt, strange one")
+
+meaty = input("Do you eat meat? (y/n):")
+
+if meaty == "y":
+    print("Here is the meaty menu...")
 else:
-    print("You are wise beyond doubt, strange one")
+    print("Here is the veggie menu...")
