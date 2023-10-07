8535c4c823f9a443aeb3052a4846d28c18c1b844
Meriem1551 <meriem200409@gmail.com>
2023-10-07 15:21:20 +0200

    Adding 'break' and 'continue' keyword

--- loops/while.py
+++ loops/while.py
@@ -2,7 +2,12 @@ age = 25
 num = 0
 
 while num < age:
+    if num == 0:
+        num += 1
+        continue
     if num % 2 == 0:
         print(num)
+    if num > 10:
+        break
     num += 1
-# nest if statement
+# keyword
