96e07cfa120441b2c2f8a9cf30ce108b5344f8b7
Meriem1551 <meriem200409@gmail.com>
2023-10-07 14:19:45 +0200

    Adding formated options

--- string_formatting.py
+++ string_formatting.py
@@ -5,4 +5,4 @@ num2 = 10.5678231
 # print("Num1 is:", num1, "and num2 is:", num2)
 
 # FORMAT METHOD
-print("num 1 is {0} and num 2 is {1}".format(num1, num2))
+print("num 1 is {0:.3} and num 2 is {1:.3}".format(num1, num2))
