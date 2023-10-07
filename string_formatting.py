37dd2c8ca06cefea84fd57d5a53f27069db98d19
Meriem1551 <meriem200409@gmail.com>
2023-10-07 14:18:07 +0200

    Adding format() method

--- string_formatting.py
+++ string_formatting.py
@@ -2,7 +2,7 @@ num1 = 3.14235676
 num2 = 10.5678231
 
 # PREVIOUS
-print("Num1 is:", num1, "and num2 is:", num2)
+# print("Num1 is:", num1, "and num2 is:", num2)
 
 # FORMAT METHOD
 print("num 1 is {0} and num 2 is {1}".format(num1, num2))
