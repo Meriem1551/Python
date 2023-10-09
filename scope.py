902762763f7ce416c6aeb7fb7193adc485fa8539
Meriem1551 <meriem200409@gmail.com>
2023-10-09 17:17:06 +0200

    Forcing a var to be global using global keyword

--- scope.py
+++ scope.py
@@ -2,7 +2,7 @@ my_name = "ryu"
 
 
 def print_name():
-    global my_name
+    global my_name  # forcing the use of yoshi as a global var
     my_name = "Yoshi"
     print("The name inside the function is: ", my_name)
 
