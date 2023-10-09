3f3cc08e50e67aae5d83b46fbb01a8612e0216dc
Meriem1551 <meriem200409@gmail.com>
2023-10-09 18:21:09 +0200

    Using set method (remove dup values)

---
+++ sorting_sets/sets.py
@@ -0,0 +1,7 @@
+nums = [1, 4, 2, 7, 3, 8, 3, 4, 8, 1]
+
+print(set(nums))  # sort and remove all the duplicated values
+
+ninjas = {"ryu": "red", "yoshi": "black", "mario": "black"}
+
+print(set(ninjas.values()))  # remove the dup values
