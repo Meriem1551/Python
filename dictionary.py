197bc5e1eb8e7439970f370b400e6c8028d306dd
Meriem1551 <meriem200409@gmail.com>
2023-10-09 18:05:26 +0200

    Create a dictionary with user input

--- dictionary.py
+++ dictionary.py
@@ -1,20 +1,41 @@
-ninja_belt = {"Crystal": "red", "ryu": "yellow"}
-# print(ninja_belt["Crystal"])
+# ninja_belt = {"Crystal": "red", "ryu": "yellow"}
+# # print(ninja_belt["Crystal"])
 
-# print("yoshi" in ninja_belt)
-# print("ryu" in ninja_belt)
+# # print("yoshi" in ninja_belt)
+# # print("ryu" in ninja_belt)
 
-# print(ninja_belt.keys())
-# print(list(ninja_belt))
-# print(ninja_belt.values())
+# # print(ninja_belt.keys())
+# # print(list(ninja_belt))
+# # print(ninja_belt.values())
 
-vals = list(ninja_belt.values())
-# print(vals.count("red"))  # return number of the vlue passed
+# vals = list(ninja_belt.values())
+# # print(vals.count("red"))  # return number of the vlue passed
 
-ninja_belt["mario"] = "blue"
+# ninja_belt["mario"] = "blue"
 
-# print(ninja_belt)
+# # print(ninja_belt)
 
-person = dict(name="Meriem", age=19, weight="44kg")
+# person = dict(name="Meriem", age=19, weight="44kg")
 
-print(person)
+# print(person)
+
+
+def ninja_intro(dictionary):
+    for key, val in dictionary.items():
+        print(f"I am {key} and i am a {val} belt")
+
+
+ninja_belts = {}
+
+while True:
+    ninja_name = input("Enter a ninja name: ")
+    ninja_belt = input("Enter a ninja colour: ")
+    ninja_belts[ninja_name] = ninja_belt
+
+    another = input("Add another?(y/n) ")
+    if another == "y":
+        continue
+    else:
+        break
+
+ninja_intro(ninja_belts)
