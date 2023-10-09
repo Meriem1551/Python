9cdeb9f2c675fe0756f730766359f34cf2bd4be7
Meriem1551 <meriem200409@gmail.com>
2023-10-09 18:31:15 +0200

    Calculating number of belts using count

--- dictionary.py
+++ dictionary.py
@@ -20,9 +20,16 @@
 # print(person)
 
 
-def ninja_intro(dictionary):
-    for key, val in dictionary.items():
-        print(f"I am {key} and i am a {val} belt")
+# def ninja_intro(dictionary):
+#     for key, val in dictionary.items():
+#         print(f"I am {key} and i am a {val} belt")
+
+
+def belt_count(dictionary):
+    belts = list(dictionary.values())
+    for belt in belts:
+        num = belts.count(belt)
+        print(f"There are {num} {belt} belts")
 
 
 ninja_belts = {}
@@ -38,4 +45,5 @@ while True:
     else:
         break
 
-ninja_intro(ninja_belts)
+belt_count(ninja_belts)
+# ninja_intro(ninja_belts)
