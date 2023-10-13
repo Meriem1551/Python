5a966800b705fa7c21dcc232d650e494baa977b6
Meriem1551 <meriem200409@gmail.com>
2023-10-13 13:30:32 +0200

    Recreating a class aa a package

---
+++ spaces/planet.py
@@ -0,0 +1,34 @@
+class Planet:
+    shape = "round"  # shared property that didn't change
+
+    def __init__(self, name, radius, gravity, system):
+        self.name = name
+        self.radius = radius
+        self.gravity = gravity
+        self.system = system
+
+    def orbit(self):
+        return f"{self.name} is orbiting in the {self.system}"
+
+    @classmethod
+    def commons(cls):
+        return f"All planets are {cls.shape} because of gravity"
+
+    @staticmethod
+    def spin(speed):
+        return f"All planets spins and spins at {speed}"
+
+
+hoth = Planet("Hoth", 2000000, 5.5, "Hoth System")
+print(f"Name is: {hoth.name}")
+print(f"Radius is: {hoth.radius}")
+print(f"Gravity is: {hoth.gravity}")
+print(f"System is: {hoth.system}")
+
+print(hoth.orbit())
+
+# init func passing custom values
+# print(naboo.orbit())
+# we can do instance.classmethod but not classname.instancemethod
+print(Planet.commons())
+# same for static methods
