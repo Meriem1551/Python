d1e07105224af40fa715409595de33498dd0a274
Meriem1551 <meriem200409@gmail.com>
2023-10-13 12:06:13 +0200

    Adding a static and a class methods

--- classes.py
+++ classes.py
@@ -1,4 +1,6 @@
 class Planet:
+    shape = "round"  # shared property that didn't change
+
     def __init__(self, name, radius, gravity, system):
         self.name = name
         self.radius = radius
@@ -8,14 +10,22 @@ class Planet:
     def orbit(self):
         return f"{self.name} is orbiting in the {self.system}"
 
+    @classmethod
+    def commons(cls):
+        return f"All planets are {cls.shape} because of gravity"
 
-hoth = Planet("Hoth", 2000000, 5.5, "Hoth System")
-print(f"Name is: {hoth.name}")
-print(f"Radius is: {hoth.radius}")
-print(f"Gravity is: {hoth.gravity}")
-print(f"System is: {hoth.system}")
+    @staticmethod
+    def spin(speed):
+        return f"All planets spins and spins at {speed}"
 
-print(hoth.orbit())
+
+# hoth = Planet("Hoth", 2000000, 5.5, "Hoth System")
+# print(f"Name is: {hoth.name}")
+# print(f"Radius is: {hoth.radius}")
+# print(f"Gravity is: {hoth.gravity}")
+# print(f"System is: {hoth.system}")
+
+# print(hoth.orbit())
 
 # init func passing custom values
 naboo = Planet("Naboo", 300000, 8, "Naboo System")
@@ -24,3 +34,7 @@ print(f"Radius is: {naboo.radius}")
 print(f"Gravity is: {naboo.gravity}")
 print(f"System is: {naboo.system}")
 print(naboo.orbit())
+# we can do instance.classmethod but not classname.instancemethod
+print(Planet.commons())
+# same for static methods
+print(Planet.spin("a very high speed"))
