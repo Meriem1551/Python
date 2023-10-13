8727083ddfabdc185424d07184845082332b551b
Meriem1551 <meriem200409@gmail.com>
2023-10-13 11:27:44 +0200

    Creaing instance

--- classes.py
+++ classes.py
@@ -5,9 +5,14 @@ class Planet:
         self.gravity = 5.5
         self.system = "Horh System"
 
+    def orbit(self):
+        return f"{self.name} is orbiting in the {self.system}"
+
 
 hoth = Planet()
 print(f"Name is: {hoth.name}")
 print(f"Radius is: {hoth.radius}")
 print(f"Gravity is: {hoth.gravity}")
 print(f"System is: {hoth.system}")
+
+print(hoth.orbit())
