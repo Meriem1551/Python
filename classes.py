90ba580d5b12b8028c4eaf6cc1c910c75cbb3fac
Meriem1551 <meriem200409@gmail.com>
2023-10-13 13:30:50 +0200

    Import classes and functions

--- classes.py
+++ classes.py
@@ -1,34 +1,11 @@
-class Planet:
-    shape = "round"  # shared property that didn't change
+from spaces.planet import Planet
+from spaces.calc import planet_mass, planet_vol
 
-    def __init__(self, name, radius, gravity, system):
-        self.name = name
-        self.radius = radius
-        self.gravity = gravity
-        self.system = system
+from classes import Planet
 
-    def orbit(self):
-        return f"{self.name} is orbiting in the {self.system}"
+# from functions import first
+naboo = Planet("Naboo", 300000, 8, "Naboo System")
+naboo_mass = planet_mass(naboo.gravity, naboo.radius)
+naboo_vol = planet_vol(naboo.radius)
 
-    @classmethod
-    def commons(cls):
-        return f"All planets are {cls.shape} because of gravity"
-
-    @staticmethod
-    def spin(speed):
-        return f"All planets spins and spins at {speed}"
-
-
-# hoth = Planet("Hoth", 2000000, 5.5, "Hoth System")
-# print(f"Name is: {hoth.name}")
-# print(f"Radius is: {hoth.radius}")
-# print(f"Gravity is: {hoth.gravity}")
-# print(f"System is: {hoth.system}")
-
-# print(hoth.orbit())
-
-# init func passing custom values
-# print(naboo.orbit())
-# we can do instance.classmethod but not classname.instancemethod
-# print(Planet.commons())
-# same for static methods
+print(f"{naboo.name} has a mass of {naboo_mass} and a volume of {naboo_vol}")
