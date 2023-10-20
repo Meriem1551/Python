e15b3de36f7df76efdc97a97237db41f3927790b
Meriem1551 <meriem200409@gmail.com>
2023-10-20 18:13:46 +0200

    Reading dna file using with  as

--- read.py
+++ read.py
@@ -9,8 +9,17 @@ ipsum_file = open("files/ipsum.txt")
 # lines = ipsum_file.readlines()
 # print(lines)
 
-ipsum_file.seek(50)  # start from pos 50
-file_text = ipsum_file.read(100)  # return 100 chars from pos 50
-print(file_text)
+# ipsum_file.seek(50)  # start from pos 50
+# file_text = ipsum_file.read(100)  # return 100 chars from pos 50
+# print(file_text)
 
-ipsum_file.close()
+# ipsum_file.close()
+
+
+def sequence_filter(line):
+    return ">" not in line
+
+
+with open("files/dna_sequence.txt") as dna_file:
+    lines = dna_file.readlines()
+    print(list(filter(sequence_filter, lines)))
