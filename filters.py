372a393f4bab10ac8f6ab056bd57975220bcb60b
Meriem1551 <meriem200409@gmail.com>
2023-10-20 15:20:05 +0200

    Filterinf a list using for loop

--- filters.py
+++ filters.py
@@ -5,4 +5,10 @@ def remove_fails(grade):
     return grade != "F"
 
 
-print(list(filter(remove_fails, grades)))
+# print(list(filter(remove_fails, grades)))
+
+filtered_grades = []
+for grade in grades:
+    if grade != "F":
+        filtered_grades.append(grade)
+print(filtered_grades)
