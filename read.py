06d2423ba8d348fd4188f7232a489dd3cf82561b
Meriem1551 <meriem200409@gmail.com>
2023-10-20 17:50:16 +0200

    Reading file from specific pos and specific number of characters

--- read.py
+++ read.py
@@ -6,5 +6,11 @@ ipsum_file = open("files/ipsum.txt")
 
 # ipsum_file.seek(100)#puting the cursor at pos 100
 
-lines = ipsum_file.readlines()
-print(lines)
+# lines = ipsum_file.readlines()
+# print(lines)
+
+ipsum_file.seek(50)  # start from pos 50
+file_text = ipsum_file.read(100)  # return 100 chars from pos 50
+print(file_text)
+
+ipsum_file.close()
