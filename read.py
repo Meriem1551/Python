836b76fd8399ec343ba08a2f2c0170b2706f6602
Meriem1551 <meriem200409@gmail.com>
2023-10-20 17:46:23 +0200

    Reading files using readlines method

--- read.py
+++ read.py
@@ -1,4 +1,10 @@
 ipsum_file = open("files/ipsum.txt")
 
-for line in ipsum_file:
-    print(line.rstrip())
+
+# for line in ipsum_file:
+#     print(line.rstrip())
+
+# ipsum_file.seek(100)#puting the cursor at pos 100
+
+lines = ipsum_file.readlines()
+print(lines)
