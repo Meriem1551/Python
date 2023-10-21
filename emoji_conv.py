209f5f0029b15f25ce3ac80012c2ff9840ce2243
Meriem1551 <meriem200409@gmail.com>
2023-10-21 21:46:21 +0200

    Creating function to convert emojies

--- emoji_conv.py
+++ emoji_conv.py
@@ -1,7 +1,11 @@
+def emoji_convertor(message):
+    words = message.split(" ")
+    emojis = {":)": "😄", ":(": "🙁", "xD": "😆", "<3": "❤"}
+    output = ""
+    for word in words:
+        output += " " + emojis.get(word, word)
+    return output
+
+
 message = input(">")
-words = message.split(" ")
-emojis = {":)": "😄", ":(": "🙁", "xD": "😆", "<3": "❤"}
-output = ""
-for word in words:
-    output += " " + emojis.get(word, word)
-print(output)
+print(emoji_convertor(message))
